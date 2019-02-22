f579ddaf8294f91a9f709d5712a9bf83e624e217
Jonas Reinwald <jonas.reinwald@htwg-konstanz.de>
2019-02-22 19:47:55 +0100

    improve listing

--- code/chapter_005_styled_components.js
+++ code/chapter_005_styled_components.js
@@ -1,6 +1,9 @@
 // ThemedEdit can be used like a regular React component
 const ThemedEdit = styled.input`
-    color: ${(props: SimpleThemeProps) => props.theme.color}; // properties depending on theme
-    width: 100%; // static css property
-    ... // additional css properties;
+    // properties depending on theme (dynamic)
+    color: ${(props: SimpleThemeProps) => props.theme.color};
+    // static css property
+    width: 100%;
+    // additional css properties
+    ...
 `;
